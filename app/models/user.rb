cd8b8aa5c698642547e09cf4728e20acfb43d2c1
Alice Freda <alicefreda@gmail.com>
2020-04-06 16:39:08 -0700

    add user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :projects, dependent: :destroy #if a user is deleted, the project they created is as well
+
 end
