4885667b17ea840c35ea30d117afd17d28b73a92
Alice Freda <alicefreda@gmail.com>
2020-04-06 16:39:20 -0700

    update root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   resources :projects
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
-
+  root to: 'projects#index'
+  
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   devise_for :users
-  root to: 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
