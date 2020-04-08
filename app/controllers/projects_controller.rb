e27007996873da53e144dfa25f69918c6c531a86
Alice Freda <alicefreda@gmail.com>
2020-04-08 16:33:59 -0700

    add description to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -61,6 +61,6 @@ class ProjectsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def project_params
-      params.require(:project).permit(:title, :donation_goal, :user_id)
+      params.require(:project).permit(:title, :donation_goal, :user_id, :description)
     end
 end
