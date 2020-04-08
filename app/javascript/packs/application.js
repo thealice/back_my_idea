845eb970d0ecdeb4e193b044e42b43962987b79e
Alice Freda <alicefreda@gmail.com>
2020-04-08 16:16:21 -0700

    install action text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,8 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
 import "stylesheets/application"
