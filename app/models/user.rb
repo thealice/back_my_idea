1db9eaf33d3fc0b00a60f9908800a8b13311cc2c
Alice Freda <alicefreda@gmail.com>
2020-04-09 14:17:37 -0700

    add omniauth login for stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_many :projects, dependent: :destroy #if a user is deleted, the project they created is as well
+
+  def can_receive_payments?
+    uid? &&  provider? && access_code? && publishable_key?
+  end
 
 end
